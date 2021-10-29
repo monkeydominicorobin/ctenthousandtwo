343ea8d4d309c8886ef704d54b6cd2e6826882f5
Rommel Dominguez <bm-2cu8dmto5ga5pf2xkvapdrqcpwtzybsxpw@riseup.net>
2021-10-29 21:09:06 +0800

    how far is this going to go

--- tenktwo.c
+++ tenktwo.c
@@ -17,13 +17,20 @@ char* fThree()
 	return "Function Three";
 }
 
+char* fFour()
+{
+	return "Function Four";
+}
+
 int main(int argc, char* argv[])
 {
 	char* sa = fOne();
 	char* sb = fTwo();
 	char* sc = fThree();
+	char* sd = fFour();
 	printf("%s\n", sa);
         printf("%s\n", sb);
 	printf("%s\n", sc);
+	printf("%s\n", sd);
 	return 0;
 }
