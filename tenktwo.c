1fa518fc4a5ebe492147d3ef45f8526677d6de1c
Rommel Dominguez <bm-2cu8dmto5ga5pf2xkvapdrqcpwtzybsxpw@riseup.net>
2021-10-29 21:02:08 +0800

    this is more like js

--- tenktwo.c
+++ tenktwo.c
@@ -12,11 +12,18 @@ char* fTwo()
 	return "Function Two";
 }
 
+char* fThree()
+{
+	return "Function Three";
+}
+
 int main(int argc, char* argv[])
 {
 	char* sa = fOne();
 	char* sb = fTwo();
+	char* sc = fThree();
 	printf("%s\n", sa);
-        printf("%s\n", sb);	
+        printf("%s\n", sb);
+	printf("%s\n", sc);
 	return 0;
 }
