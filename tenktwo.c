a0e29321c3392d88bf0dd853f8bf23fa0a558a94
Rommel Dominguez <bm-2cu8dmto5ga5pf2xkvapdrqcpwtzybsxpw@riseup.net>
2021-10-30 17:07:31 +0800

    call me maybe

--- tenktwo.c
+++ tenktwo.c
@@ -22,15 +22,22 @@ char* fFour()
 	return "Function Four";
 }
 
+char* fFive()
+{
+	return "Function Five";
+}
+
 int main(int argc, char* argv[])
 {
 	char* sa = fOne();
 	char* sb = fTwo();
 	char* sc = fThree();
 	char* sd = fFour();
+	char* se = fFive();
 	printf("%s\n", sa);
         printf("%s\n", sb);
 	printf("%s\n", sc);
 	printf("%s\n", sd);
+	printf("%s\n", se);
 	return 0;
 }
